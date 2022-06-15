00488ee787521b1bbde991ae36cb52a58929985d
aamir-poswal <aamir.pucit@gmail.com>
2022-06-15 20:27:54 +0200

    css issue resolved

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,15 @@ module.exports = {
         },
       },
       {
-        test: /\.scss$/,
-        use: ["style-loader", "sass-loader"],
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
       },
       {
         test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)$/,
