53f9751a24663e5a9582eca286a0d59be1cf38e4
aamir-poswal <aamir.pucit@gmail.com>
2022-06-15 17:02:14 +0200

    fix for loading scss file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   output: {
@@ -20,9 +19,19 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: ["css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif|woff|woff2|eot|ttf|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
       },
     ],
   },
-  plugins: [new MiniCssExtractPlugin()],
 };
