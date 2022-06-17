866c82dcd55a944e3ecada9b411cc3e0787eb056
aamir-poswal <aamir.pucit@gmail.com>
2022-06-17 19:22:15 +0200

    added some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
 
 export function App() {
-    return (
-        <div>
-            <h1>React App</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos delectus voluptatem ab repellat saepe eos molestiae cupiditate, omnis doloremque, necessitatibus totam consectetur porro sint quam libero aperiam? Laborum, quaerat?</p>
+  return (
+    <div>
+      <div className="container">
+        <div className="row row-action-height">
+          <button className="button-23">Download KML</button>
         </div>
-    );
+        <div className="row row-content-height">
+          <p>these are contents</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,35 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index.bundle.js',
-    },
-    devServer: {
-        port: 3010,
-        watchContentBase: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            }
-        ]
-    },
-    plugins: [new MiniCssExtractPlugin()],
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.bundle.js",
+  },
+  devServer: {
+    port: 3010,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
 };
