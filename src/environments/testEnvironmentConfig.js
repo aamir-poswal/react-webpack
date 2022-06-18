23f516c739b3f22443ecd8ca06a0eb2c3b89aecb
aamir-poswal <aamir.pucit@gmail.com>
2022-06-18 20:06:01 +0200

    added ResultService.js

--- src/environments/testEnvironmentConfig.js
+++ src/environments/testEnvironmentConfig.js
@@ -1,6 +1,4 @@
-//   export const REACT_APP_TEST_KEY = process.test.env.REACT_APP_TEST_KEY;
 const config = {
-  REACT_APP_TEST_KEY: process.env.REACT_APP_TEST_KEY,
   fmeCloudServerBaseURL: "https://volue-geminitest.fmecloud.com",
   fmeUserName: process.env.REACT_APP_FME_USERNAME,
   fmePassword: process.env.REACT_APP_FME_PASSWORD,
