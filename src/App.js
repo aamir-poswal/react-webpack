f7a2f063ae0e14271fdecd307968f982053b05ba
aamir-poswal <aamir.pucit@gmail.com>
2022-06-19 14:06:35 +0200

    read test token from user service

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
 import Report from "./components/Report";
-import config from "./environments/TestEnvironmentConfig";
+import UserContext from "./UserContext";
 import userService from "./services/userService";
+
 export function App() {
+  var [token, setToken] = useState("");
   useEffect(() => {
-    userService.loginToFMEServer();
+    var token = userService.loginToFMEServer();
+    setToken(token);
   }, []);
   return (
     <div>
       <div className="container">
         <div>
           <div className="row row-action-height">
-            <button className="button-23">Download KML</button>
+            <button className="button-23">Download KML {token}</button>
           </div>
           <Report></Report>
         </div>
