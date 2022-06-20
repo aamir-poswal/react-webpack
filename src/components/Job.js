08564934b049137d2fd6503339ec8e15a2812004
aamir-poswal <aamir.pucit@gmail.com>
2022-06-20 14:40:27 +0200

    added input export to html and kml

--- src/components/Job.js
+++ src/components/Job.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Job() {
+  var [exportToHTML, setExportToHTML] = useState(true);
+  var [exportToKML, setExportToKML] = useState(true);
+
   return (
     <div className="container fluid">
-      <div className="row">
-        <div className="col-sm col-md col-lg">
-          <p>This is test content</p>
-        </div>
-        <div className="col-sm col-md col-lg">
-          <p>This is test content</p>
-        </div>
+      <div className="form-check pt-5">
+        <input
+          type="checkbox"
+          className="form-check-input"
+          id="checkboxExportToHTML"
+          checked={exportToHTML}
+          onChange={() => setExportToHTML(!exportToHTML)}
+        />
+        <label className="form-check-label" htmlFor="checkboxExportToHTML">
+          Export To HTML
+        </label>
+      </div>
+      <div className="form-check">
+        <input
+          type="checkbox"
+          className="form-check-input"
+          id="checkboxExportToKML"
+          checked={exportToKML}
+          onChange={() => setExportToKML(!exportToKML)}
+        />
+        <label className="form-check-label" htmlFor="checkboxExportToKML">
+          Export To KML
+        </label>
+      </div>
+      <div className="pt-2">
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </div>
     </div>
   );
