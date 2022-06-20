5e06993ba87d48dda7632cb78caf23b7cbb2afca
aamir-poswal <aamir.pucit@gmail.com>
2022-06-20 16:22:57 +0200

    access job id from useContext in report component

--- src/components/Report.js
+++ src/components/Report.js
@@ -1,8 +1,13 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import UserContext from "../UserContext";
+import JobContext from "../JobContext";
 import ReactDOM from "react-dom";
 const Report = () => {
   const user = useContext(UserContext);
+  const jobId = useContext(JobContext);
+  useEffect(() => {
+    console.log(`job id in report component ${jobId}`);
+  }, []);
   return (
     <div>
       <div className="row row-action-height">
