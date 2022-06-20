3231d6101c652e8ac200d082ab17c460b8704408
aamir-poswal <aamir.pucit@gmail.com>
2022-06-20 14:47:37 +0200

    horizontal form

--- src/components/Job.js
+++ src/components/Job.js
@@ -6,34 +6,40 @@ function Job() {
 
   return (
     <div className="container fluid">
-      <div className="form-check pt-5">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="checkboxExportToHTML"
-          checked={exportToHTML}
-          onChange={() => setExportToHTML(!exportToHTML)}
-        />
-        <label className="form-check-label" htmlFor="checkboxExportToHTML">
-          Export To HTML
-        </label>
-      </div>
-      <div className="form-check">
-        <input
-          type="checkbox"
-          className="form-check-input"
-          id="checkboxExportToKML"
-          checked={exportToKML}
-          onChange={() => setExportToKML(!exportToKML)}
-        />
-        <label className="form-check-label" htmlFor="checkboxExportToKML">
-          Export To KML
-        </label>
-      </div>
-      <div className="pt-2">
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
+      <div className="row">
+        <div className="col pt-4">
+          <div className="form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="checkboxExportToHTML"
+              checked={exportToHTML}
+              onChange={() => setExportToHTML(!exportToHTML)}
+            />
+            <label className="form-check-label" htmlFor="checkboxExportToHTML">
+              Export To HTML
+            </label>
+          </div>
+        </div>
+        <div className="col pt-4">
+          <div className="form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="checkboxExportToKML"
+              checked={exportToKML}
+              onChange={() => setExportToKML(!exportToKML)}
+            />
+            <label className="form-check-label" htmlFor="checkboxExportToKML">
+              Export To KML
+            </label>
+          </div>
+        </div>
+        <div className="col pt-4">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
       </div>
     </div>
   );
