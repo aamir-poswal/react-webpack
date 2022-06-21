03989f00b67fc036e0caab6f4ca3b135e1c7b351
aamir-poswal <aamir.pucit@gmail.com>
2022-06-21 15:10:43 +0200

    download kml file

--- src/services/ResultService.js
+++ src/services/ResultService.js
@@ -21,7 +21,7 @@ const downloadHTMLReport = async (jobId, token) => {
 
 const downloadKML = async (jobId, token) => {
   console.log(`at the start of downloadKML jobId ${jobId}`);
-  response = await fetch(
+  let response = await fetch(
     `${config.fmeCloudServerBaseURL}/fmerest/v3/resources/connections/FME_SHAREDRESOURCE_DATA/filesys/GeminiWaterAnalysisOutput/FireFlowReport.kml`,
     {
       headers: {
@@ -34,6 +34,14 @@ const downloadKML = async (jobId, token) => {
   ).catch(function () {
     console.log("error");
   });
+  let blob = await response.blob();
+  let url = window.URL.createObjectURL(blob);
+  let a = document.createElement("a");
+  a.href = url;
+  a.download = "FireFlowReport.kml";
+  document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
+  a.click();
+  a.remove();
 
   console.log(`at the end of downloadKML jobId ${jobId}`);
 };
