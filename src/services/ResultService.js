dfb0b6463816c0c2539fc532cf8de4953f5161bf
aamir-poswal <aamir.pucit@gmail.com>
2022-06-21 14:26:16 +0200

    result service methods updated

--- src/services/ResultService.js
+++ src/services/ResultService.js
@@ -1,47 +1,43 @@
 import config from "../environments/TestEnvironmentConfig";
 
-const downloadHTMLReport = (jobId, token) => {
+const downloadHTMLReport = async (jobId, token) => {
   const data = { jobId: jobId };
   console.log(`at the start of downloadHTMLReport jobId ${jobId}`);
-  fetch(
+  var response = await fetch(
     `${config.fmeCloudServerBaseURL}/fmerest/v3/transformations/transact/GeminiWaterAnalysis/GeminiWaterAnalysis_FireFlowReport_DataDownload.fmw`,
     {
       headers: {
         "Content-Type": "application/json;charset=UTF-8",
         Accept: "application/json",
+        Authorization: `fmetoken token=${token}`,
       },
       method: "POST",
       body: JSON.stringify(data),
-      mode: "no-cors",
     }
-  )
-    .then((response) => console.log(response))
-    .catch(function () {
-      console.log("error");
-    });
+  ).catch(function () {
+    console.log("error");
+  });
 
   console.log(`at the end of downloadHTMLReport jobId ${jobId}`);
 };
 
-const downloadKML = (jobId, token) => {
+const downloadKML = async (jobId, token) => {
   const data = { jobId: jobId };
   console.log(`at the start of downloadKML jobId ${jobId}`);
-  fetch(
+  var response = fetch(
     `${config.fmeCloudServerBaseURL}/fmerest/v3/transformations/transact/GeminiWaterAnalysis/GeminiWaterAnalysis_FireFlowReport_DataDownload.fmw`,
     {
       headers: {
         "Content-Type": "application/json;charset=UTF-8",
         Accept: "application/json",
+        Authorization: `fmetoken token=${token}`,
       },
       method: "POST",
       body: JSON.stringify(data),
-      mode: "no-cors",
     }
-  )
-    .then((response) => console.log(response))
-    .catch(function () {
-      console.log("error");
-    });
+  ).catch(function () {
+    console.log("error");
+  });
 
   console.log(`at the end of downloadKML jobId ${jobId}`);
 };
