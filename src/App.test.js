6125b4b2731d8e5ed1b6e7cdfb5079003d1a450d
aamir-poswal <aamir.pucit@gmail.com>
2022-07-11 23:09:21 +0200

    test user service get token test

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,36 @@
+import "whatwg-fetch";
 import React from "react";
-import { render, screen } from "@testing-library/react";
-
+import {
+  render,
+  screen,
+  waitFor,
+  findByText,
+  getByText,
+} from "@testing-library/react";
+import { setupServer } from "msw/node";
+import { rest } from "msw";
+import "@testing-library/jest-dom/extend-expect";
 import App from "./App";
-import { debug } from "webpack";
+import UserService from "./services/UserService";
+
+const server = setupServer(
+  rest.post(
+    "https://volue-geminitest.fmecloud.com/fmetoken/service/generate",
+    (req, res, ctx) => {
+      return res(ctx.status(200), ctx.text("1254"));
+    }
+  )
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe("App", () => {
-  test("renders App component", () => {
+  test("renders App component", async () => {
+    await UserService.loginToFMEServer();
     render(<App />);
-    screen.findByText("Something");
+
     screen.debug();
-    // expect(screen.getByText("Something")).toBeInTheDocument();
   });
 });
